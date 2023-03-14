eeefe63602d34c7243240c6a881d21b60e43b70d
BrianKheng <briankheng63@gmail.com>
2023-03-14 13:35:59 +0700

    add: operator overloading (+=, -=)

--- src/InventoryHolder/InventoryHolder.hpp
+++ src/InventoryHolder/InventoryHolder.hpp
@@ -4,22 +4,26 @@
 #include <vector>
 #include "../Values/Card.hpp"
 
-class InventoryHolder{ // ! belum abstact
+class InventoryHolder
+{ // ! belum abstact
 private:
     std::vector<Card> cards;
     const int limit;
+
 public:
     InventoryHolder(int);
-    InventoryHolder(const InventoryHolder&);
+    InventoryHolder(const InventoryHolder &);
     ~InventoryHolder();
-    InventoryHolder operator= (const InventoryHolder&); // ! childnya harus bikin lagi, gak DRY
+    InventoryHolder operator=(const InventoryHolder &); // ! childnya harus bikin lagi, gak DRY
     void insertCard(Card);
     void removeCard(Card);
     Card getItem(int);
     int getSize();
-    friend InventoryHolder operator+ (InventoryHolder&, Card);
-    friend InventoryHolder operator- (InventoryHolder&, Card);
-    friend std::istream& operator>> (std::istream&, InventoryHolder&);
+    friend InventoryHolder operator+(InventoryHolder &, Card);
+    friend InventoryHolder operator-(InventoryHolder &, Card);
+    friend InventoryHolder &operator+=(InventoryHolder &, Card);
+    friend InventoryHolder &operator-=(InventoryHolder &, Card);
+    friend std::istream &operator>>(std::istream &, InventoryHolder &);
 };
 
 #endif
