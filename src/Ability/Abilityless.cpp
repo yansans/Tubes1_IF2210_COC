85ee84c63069ef3d6d7e41631d0509139ade5be9
rayhanp1402 <rayhan.hanif14maulana@gmail.com>
2023-03-14 15:26:05 +0700

    Abilityless

--- src/Ability/Abilityless.cpp
+++ src/Ability/Abilityless.cpp
@@ -5,6 +5,19 @@ Abilityless::Abilityless() : Ability("ABILITYLESS") {}
 Abilityless::~Abilityless() {}
 
 void Abilityless::executeAbility(Player& player, vector<Player*> players, map<Player*, Ability*> playerAbility) {
+    bool allAbilityUsed = true;
+
+    for(auto it = playerAbility.begin(); it != playerAbility.end(); it++) {
+        if(!it->second->getIsUsed() || !it->second->getIsDisabled()) {
+            allAbilityUsed = false;
+        }
+    }
+
+    if(allAbilityUsed) {
+        playerAbility[&player]->setIsDisabled(true);
+        playerAbility[&player]->setIsUsed(true);
+        cout << "Eits, ternyata semua pemain sudah memakai kartu kemampuan. Yah kamu kena sendiri deh, kemampuanmu menjadi abilityless. Yah, pengunaan kartu ini sia-sia";
+    }
     cout << player.getName() << " akan mematikan kartu ability lawan!" << endl;
     cout << "Silahkan pilih pemain yang kartu abilitynya ingin dimatikan:" << endl;
 
@@ -20,5 +33,6 @@ void Abilityless::executeAbility(Player& player, vector<Player*> players, map<Pl
     cin >> number;
     
     playerAbility[temp[number]]->setIsDisabled(true);
+    playerAbility[&player]->setIsUsed(true);
     cout << "Kartu ability pemain " << temp[number] << " telah dimatikan.";
 }
